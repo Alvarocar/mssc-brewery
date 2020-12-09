ac268cf9e7e3d9755e353c8352deee12ccdf06d7
Alvaro Cardozo <alvaro_car6@hotmail.com>
2020-12-09 18:22:27 -0500

    first commit

---
+++ src/main/java/dev/alvarocar/msscbrawery/MsscBraweryApplication.java
@@ -0,0 +1,13 @@
+package dev.alvarocar.msscbrawery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBraweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBraweryApplication.class, args);
+	}
+
+}
