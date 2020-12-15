4d3e34c8bfc02d58854f7aabfd8ae491aade4fa3
Alvaro Cardozo <alvaro_car6@hotmail.com>
2020-12-15 16:47:55 -0500

    metodo handlepost

--- src/main/java/dev/alvarocar/msscbrawery/business/controller/BeerController.java
+++ src/main/java/dev/alvarocar/msscbrawery/business/controller/BeerController.java
@@ -2,12 +2,10 @@ package dev.alvarocar.msscbrawery.business.controller;
 
 import dev.alvarocar.msscbrawery.business.service.beer.BeerService;
 import dev.alvarocar.msscbrawery.model.dto.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -24,5 +22,13 @@ public class BeerController {
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(BeerDto beerDto){
+        BeerDto newBeer = beerService.saveNewBeer(beerDto);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("location", "/api/v1/beer/"+ newBeer.getId().toString());
+        return new ResponseEntity(httpHeaders, HttpStatus.CREATED);
     }
 }
